3903f807c27bd936c80c3ebde2afd64006f65fe2
Cheng Yi Chao <onesuperclark@gmail.com>
2012-03-21 10:39:01 +0800

    use lineTo to sketch

--- grafic.js
+++ grafic.js
@@ -8,29 +8,36 @@
 
 function sketchProc(p) {
 
-	var nX, nY;
+	var nowX, nowY;
+	var lastX, lastY;
+
 	var mouseDown = false;
     
-	//global attributes 
-	p.strokeSize = 10.0;
-	p.strokeColor = p.color(0);	// default black
+	// default attributes 
+	p.strokeSize = 5.0;
+	p.strokeColor = p.color(0);	
 	p.strokeOpacity = 255;
 	
 	
     p.setup = function() {
 		p.size(800, 600);
 		p.background(255); // white canvas	
-		p.noStroke();
 		p.frameRate(60);
-		nX = 0;
-		nY = 0;
+		nowX = 0;
+		nowY = 0;
+		lastX = 0;
+		lastY = 0;
     }
-
 
     p.draw = function() {
 		if (mouseDown ) {
-			p.fill(p.strokeColor, p.strokeOpacity);
-			p.ellipse(nX, nY, p.strokeSize, p.strokeSize);
+			//p.fill(p.strokeColor, p.strokeOpacity);
+			//p.ellipse(nX, nY, p.strokeSize, p.strokeSize);
+			p.strokeWeight(p.strokeSize);
+			p.stroke(p.strokeColor);
+			p.line(lastX, lastY, nowX, nowY);
+			lastX = nowX;
+			lastY = nowY;
 		}
     }
 
@@ -40,8 +47,8 @@ function sketchProc(p) {
     }
 
 	p.mouseDragged = function() {
-		nX = p.mouseX;
-		nY = p.mouseY;	
+		nowX = p.mouseX;
+		nowY = p.mouseY;	
 	}
 	
 	p.mouseClicked = function() {
@@ -50,8 +57,10 @@ function sketchProc(p) {
 
 	p.mousePressed = function() {
 		mouseDown = true;
-		nX = p.mouseX;
-		nY = p.mouseY;
+		nowX = p.mouseX;
+		nowY = p.mouseY;
+		lastX = p.mouseX;
+		lastY = p.mouseY;
 	}
 	p.mouseReleased = function() {
 		mouseDown = false;
