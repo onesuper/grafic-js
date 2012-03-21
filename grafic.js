2b7a02d26ab8a29ae774b6b58c27905cd7dee235
Cheng Yi Chao <onesuperclark@gmail.com>
2012-03-21 17:06:08 +0800

    first commit, really

--- grafic.js
+++ grafic.js
@@ -6,19 +6,24 @@
 */
 
 
+// ban the text cursor when the mouse is down in canvas
+var canvas = document.getElementById("canvas1");
+canvas.onmousedown = function() {return false;}
+
 function sketchProc(p) {
 
 	var nowX, nowY;
 	var lastX, lastY;
-
-	var mouseDown = false;
+	var pathQueue = new Queue();
+	var inStroke = false;
     
 	// default attributes 
-	p.strokeSize = 5.0;
+	p.strokeSize = 1.0;
 	p.strokeColor = p.color(0);	
 	p.strokeOpacity = 255;
 	
 	
+
     p.setup = function() {
 		p.size(800, 600);
 		p.background(255); // white canvas	
@@ -27,43 +32,44 @@ function sketchProc(p) {
 		nowY = 0;
 		lastX = 0;
 		lastY = 0;
+		
     }
 
     p.draw = function() {
-		if (mouseDown ) {
-			//p.fill(p.strokeColor, p.strokeOpacity);
-			//p.ellipse(nX, nY, p.strokeSize, p.strokeSize);
+
+		if(inStroke) {
 			p.strokeWeight(p.strokeSize);
 			p.stroke(p.strokeColor);
-			p.line(lastX, lastY, nowX, nowY);
-			lastX = nowX;
-			lastY = nowY;
-		}
+			p.smooth();
+			p.line(p.pmouseX, p.pmouseY, p.mouseX, p.mouseY);
+
+			if (pathQueue.getLength() <= 100) {
+				pathQueue.enqueue(new Array(p.mouseX, p.mouseY));
+			}
+
+			
+			while (pathQueue.getLength() >= 50) {
+				var aPoint = pathQueue.dequeue();
+				p.strokeWeight(1.0);
+				p.stroke(p.strokeColor);
+				p.line(aPoint[0], aPoint[1], p.mouseX, p.mouseY);
+			}
+
+
+		}	
     }
 
 
-    p.mouseMoved = function() {
-		
-    }
 
 	p.mouseDragged = function() {
-		nowX = p.mouseX;
-		nowY = p.mouseY;	
+		inStroke = true;
+
 	}
 	
-	p.mouseClicked = function() {
-		
-	}
 
-	p.mousePressed = function() {
-		mouseDown = true;
-		nowX = p.mouseX;
-		nowY = p.mouseY;
-		lastX = p.mouseX;
-		lastY = p.mouseY;
-	}
+
 	p.mouseReleased = function() {
-		mouseDown = false;
+		inStroke = false;
 	}
 
 }
