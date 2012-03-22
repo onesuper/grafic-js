bcbcb46444eec5bb2a1e0ce7aba757c8b8d98a45
Cheng Yi Chao <onesuperclark@gmail.com>
2012-03-22 08:50:36 +0800

    adjust the file structure

---
+++ sketch/grafic.js
@@ -0,0 +1,82 @@
+/*
+  version: 1.0.0
+  author: onesuper
+  email: onesuperclark@gmail.com
+
+*/
+
+
+// ban the text cursor when the mouse is down in canvas
+var canvas = document.getElementById("canvas1");
+canvas.onmousedown = function() {return false;}
+
+function sketchProc(p) {
+
+	var nowX, nowY;
+	var lastX, lastY;
+	var pathQueue = new Queue();
+
+    
+	// default attributes 
+	p.strokeSize = 1.0;
+	p.strokeColor = p.color(0);	
+	p.strokeOpacity = 255;
+	p.strokeTail = 50;
+
+    p.setup = function() {
+		p.size(800, 600);
+		p.background(255); // white canvas	
+		p.frameRate(60);
+		nowX = 0;
+		nowY = 0;
+		lastX = 0;
+		lastY = 0;
+    }
+
+    p.draw = function() {
+    }
+
+
+
+	p.mouseDragged = function() {
+		p.strokeWeight(p.strokeSize);
+		p.stroke(p.strokeColor);
+		p.smooth();
+		p.line(p.pmouseX, p.pmouseY, p.mouseX, p.mouseY);
+
+		if (pathQueue.getLength() <= 100) {
+			pathQueue.enqueue(new Array(p.mouseX, p.mouseY));
+		}
+
+		
+		while (pathQueue.getLength() >= p.strokeTail) {
+			var aPoint = pathQueue.dequeue();
+			p.strokeWeight(1.0);
+			p.stroke(p.strokeColor);
+			p.line(aPoint[0], aPoint[1], p.mouseX, p.mouseY);
+		}
+
+	}
+	
+	p.mousePressed = function() {
+		setStrokeSize(sld1.value);
+		setStrokeTail(sld2.value);
+	}
+
+	p.mouseReleased = function() {
+		pathQueue = new Queue();
+	}
+
+}
+
+//initalize the canvas
+var canvas = document.getElementById("canvas1");
+var processing = new Processing(canvas, sketchProc);
+//clearAll()
+
+function clearAll() {
+	//var canvas = document.getElementById("canvas1");
+	//var p = new Processing(canvas, sketchProc);
+	//p.strokeColor = p.color(0);
+	processing.setup();
+}
---
+++ sketch/setAttributes.js
@@ -0,0 +1,33 @@
+/*
+version: 1.0.0
+author: onesuper
+email: onesuperclark@gmail.com
+*/
+
+
+
+// called by the hook in the color picker
+function setStrokeColor(color)
+{
+	var p = Processing.getInstanceById("canvas1");
+	p.strokeColor = p.color(parseInt(color.substr(1, 2), 16),
+							parseInt(color.substr(3, 2), 16),
+							parseInt(color.substr(5, 2), 16)
+						   );
+	$("#colorbar .input").val(color);
+	$("#colorbar .show").css({"background" : color}); // display the selected color")
+}
+
+
+function setStrokeSize(size)
+{
+	var p = Processing.getInstanceById("canvas1");
+	p.strokeSize = size;
+}
+
+function setStrokeTail(size)
+{
+	var p = Processing.getInstanceById("canvas1");
+	p.strokeTail = size;
+}
+
