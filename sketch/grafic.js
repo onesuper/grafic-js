8c7a600a11bc36f9d4406b46f5b2109e8ca64fa7
Cheng Yi Chao <onesuperclark@gmail.com>
2012-03-23 17:22:00 +0800

    calligraph mode added

--- sketch/grafic.js
+++ sketch/grafic.js
@@ -10,12 +10,11 @@
 var canvas = document.getElementById("canvas1");
 canvas.onmousedown = function() {return false;}
 
+
+//sketch procceeding mode
 function sketchProc(p) {
 
-	var nowX, nowY;
-	var lastX, lastY;
 	var pathQueue = new Queue();
-
     
 	// default attributes 
 	p.strokeSize = 1.0;
@@ -27,10 +26,6 @@ function sketchProc(p) {
 		p.size(800, 600);
 		p.background(255); // white canvas	
 		p.frameRate(60);
-		nowX = 0;
-		nowY = 0;
-		lastX = 0;
-		lastY = 0;
     }
 
     p.draw = function() {
@@ -48,7 +43,6 @@ function sketchProc(p) {
 			pathQueue.enqueue(new Array(p.mouseX, p.mouseY));
 		}
 
-		
 		while (pathQueue.getLength() >= p.strokeTail) {
 			var aPoint = pathQueue.dequeue();
 			p.strokeWeight(1.0);
@@ -59,6 +53,7 @@ function sketchProc(p) {
 	}
 	
 	p.mousePressed = function() {
+		//set attributes everytime the mouse is pressed
 		setStrokeSize(sld1.value);
 		setStrokeTail(sld2.value);
 	}
@@ -66,17 +61,100 @@ function sketchProc(p) {
 	p.mouseReleased = function() {
 		pathQueue = new Queue();
 	}
-
 }
 
-//initalize the canvas
-var canvas = document.getElementById("canvas1");
-var processing = new Processing(canvas, sketchProc);
-//clearAll()
+
+function calligraphProc(p) {
+
+	
+    var radius = 10.0;
+	var opacity = 60;
+
+	var pathArray = new Array();
+	
+
+	// default attributes 
+	p.strokeSize = 1.0;
+	p.strokeColor = p.color(0);	
+	p.strokeOpacity = 255;
+	p.strokeTail = 50;
+
+    p.setup = function() {
+		p.size(800, 600);
+		p.background(255); // white canvas	
+		p.frameRate(30);
+    }
+
+    p.draw = function() {
+	
+		for(var i=0; i<pathArray.length; i++) {
+			p.fill(p.strokeColor, opacity);
+			p.noStroke();
+			p.smooth();
+			p.ellipse(pathArray[i][0], pathArray[i][1], radius, radius);
+		}
+		
+		if (opacity > 0) opacity -= 0.5;
+		if (radius < 30)
+			radius += 1;
+		else
+			p.noLoop();
+
+    }
+
+	p.mouseDragged = function() {
+		p.fill(p.strokeColor);
+		p.noStroke();
+		p.smooth();
+		//p.ellipse(p.mouseX, p.mouseY, 10, 10);
+		pathArray.push(new Array(p.mouseX, p.mouseY));
+	}
+	
+	p.mousePressed = function() {
+		//set attributes everytime the mouse is pressed
+		setStrokeSize(sld1.value);
+		setStrokeTail(sld2.value);
+		radius = 10.0;
+		opacity = 60;
+		p.fill(p.strokeColor);
+		p.noStroke();
+		p.smooth();
+		p.ellipse(p.mouseX, p.mouseY, radius, radius);
+		p.loop();
+
+	}
+
+	p.mouseReleased = function() {
+		pathArray = new Array();
+	
+	}
+}
+
+//initalize the sketch canvas when the app is launched 
+initSketchMode();
+
+function initSketchMode() {
+	var canvas = document.getElementById("canvas1");
+	var processing = new Processing(canvas, sketchProc);
+}
+
+
+function setSketchMode() {
+	var p = Processing.getInstanceById("canvas1");
+	p.exit();
+	var canvas = document.getElementById("canvas1");
+	var processing = new Processing(canvas, sketchProc);
+}
+
+function setCalligraphMode() {
+	var p = Processing.getInstanceById("canvas1");
+	p.exit();
+	var canvas = document.getElementById("canvas1");
+	var processing = new Processing(canvas, calligraphProc);
+}
+
 
 function clearAll() {
-	//var canvas = document.getElementById("canvas1");
-	//var p = new Processing(canvas, sketchProc);
-	//p.strokeColor = p.color(0);
+	var processing = Processing.getInstanceById("canvas1");
 	processing.setup();
 }
